186684191411469149151ad1a4247ced1688ceb0
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-05 23:16:22 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_Mi439.mk \
-    $(LOCAL_DIR)/evolution_Mi439_4_19.mk
+    $(LOCAL_DIR)/lineage_Mi439.mk \
+    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_Mi439-user \
-    evolution_Mi439-userdebug \
-    evolution_Mi439-eng \
-    evolution_Mi439_4_19-user \
+    lineage_Mi439-user \
+    lineage_Mi439-userdebug \
+    lineage_Mi439-eng \
+    lineage_Mi439_4_19-user \
     lineage_Mi439_4_19-userdebug \
-    evolution_Mi439_4_19-eng
+    Lineage_Mi439_4_19-eng
