a122696bc75d410944df9bd1c78dbd789e1777d8
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-06 14:14:02 +0600

    Update everest_Mi439_4_19.mk

--- everest_Mi439_4_19.mk
+++ everest_Mi439_4_19.mk
@@ -37,6 +37,11 @@ TARGET_VENDOR := Xiaomi
 # Signing
 # -include vendor/extra/product.mk
 
+# Extra Stuffs
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+
 
 
 # Maintainer
@@ -51,3 +56,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+
+# Gapps
+WITH_GAPPS := false 
