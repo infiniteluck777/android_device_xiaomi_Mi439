ad828f8c13b107020879f3e76e4bca4e8b2c909e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-08 14:24:30 +0600

    Update lineage_Mi439.mk

--- lineage_Mi439.mk
+++ lineage_Mi439.mk
@@ -25,6 +25,10 @@ PRODUCT_PACKAGES += \
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# Matrixx
+MATRIXX_MAINTAINER := FK_FARHAN•xD
+WITH_GMS := false
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
 PRODUCT_NAME := lineage_Mi439
